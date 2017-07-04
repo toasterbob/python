3ea0522938c206c2a4e928245a0eafc48c3594e7
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-04 17:52:03 +0530

    microblogrender_template for index.html

--- Flask/Mega-Tutorial/microblog/app/views.py
+++ Flask/Mega-Tutorial/microblog/app/views.py
@@ -12,3 +12,24 @@ def index():
 # This view is actually pretty simple, it just returns a string, to be
 # displayed on the client's web browser. The two route decorators above
 # the function create the mappings from URLs / and /index to this function.
+
+# Under the covers, the render_template function invokes the Jinja2
+# templating engine that is part of the Flask framework. Jinja2
+# substitutes {{...}} blocks with the corresponding values provided as
+# template arguments.
+
+# Control statements in templates
+
+# The Jinja2 templates also support control statements, given inside
+# {%...%} blocks. Let's add an if statement to our template
+# (file app/templates/index.html):
+  # <head>
+  #   {% if title %}
+  #   <title>{{ title }} - microblog</title>
+  #   {% else %}
+  #   <title>Welcome to microblog</title>
+  #   {% endif %}
+  # </head>
+# Now our template is a bit smarter. If the view function forgets to
+# define a page title then instead of showing an empty title the
+# template will provide its own title.
