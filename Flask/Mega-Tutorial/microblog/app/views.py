83302e3b2f7393cd817dfed3f3939535a560a6f1
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-04 17:39:18 +0530

    microbloghomepage view

--- Flask/Mega-Tutorial/microblog/app/views.py
+++ Flask/Mega-Tutorial/microblog/app/views.py
@@ -3,7 +3,17 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Mark'} # fake user
+    return '''
+    <html>
+      <head>
+        <title>Microblog Home Page</title>
+      </head>
+      <body>
+        <h1>Hello, ''' + user['nickname'] + '''!</h1>
+      </body>
+    </html>
+'''
 
 # This view is actually pretty simple, it just returns a string, to be
 # displayed on the client's web browser. The two route decorators above
