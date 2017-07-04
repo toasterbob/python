c94a291ba491be1053bf36258f0e18b856cfba2a
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-04 17:47:37 +0530

    microblogrender_template for index.html

--- Flask/Mega-Tutorial/microblog/app/views.py
+++ Flask/Mega-Tutorial/microblog/app/views.py
@@ -1,19 +1,13 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Mark'} # fake user
-    return '''
-    <html>
-      <head>
-        <title>Microblog Home Page</title>
-      </head>
-      <body>
-        <h1>Hello, ''' + user['nickname'] + '''!</h1>
-      </body>
-    </html>
-'''
+    return render_template( 'index.html',
+                            title='Home',
+                            user=user)
 
 # This view is actually pretty simple, it just returns a string, to be
 # displayed on the client's web browser. The two route decorators above
