1d1ea3bfab90a0694dee2d6f8fb2086305ba63e7
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-05 19:39:59 +0530

    for and while

--- Fundamentals1/Lists/iteration_comprehension.py
+++ Fundamentals1/Lists/iteration_comprehension.py
@@ -26,15 +26,40 @@ for char in "awesome":
 # m
 # e
 
+# Sometimes you may want to have access to the element's index in the list
+# as well as the element itself. In this case, you can pass the list into
+# the enumerate function.
+
+# You'll need to name two variables in the for loop: the first will refer
+# to the current index, the second will refer to the current element:
+
+for idx, char in enumerate("awesome"):
+    print(idx, char)
+
+# 0 a
+# 1 w
+# 2 e
+# 3 s
+# 4 o
+# 5 m
+# 6 e
 
 
+# while
+#
+# You can also do a while loop with Python, but this is a bit less common
+# when iterating
 
+i = 0
+while i < 5:
+    print(i)
+    i +=1
 
-
-
-
-
-
+# 0
+# 1
+# 2
+# 3
+# 4
 
 
 #
