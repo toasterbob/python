3eacc18468ed4699e590be2c9f2dbeff49971372
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-05 19:18:37 +0530

    for loops

---
+++ Fundamentals1/Lists/iteration_comprehension.py
@@ -0,0 +1,40 @@
+# Iterating over lists and strings
+
+# for in
+#
+# The most common way of iterating over a list is a for in loop.
+# The syntax is for ELEMENT in LIST
+# As with if statements, don't forget about the colon!
+
+values = [1,2,3,4]
+for val in values:
+    print(val)
+
+# 1
+# 2
+# 3
+# 4
+
+for char in "awesome":
+    print(char)
+
+# a
+# w
+# e
+# s
+# o
+# m
+# e
+
+
+
+
+
+
+
+
+
+
+
+
+#
