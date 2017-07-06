7e010ef00d37e0ee2af3969a2d00ca147b172025
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-06 11:08:14 +0530

    boolean exercises

---
+++ Fundamentals1/Intro/boolean_exercises.py
@@ -0,0 +1,14 @@
+#1
+first = "Hello World"
+#2
+# This is a comment
+#3
+print("I AM A COMPUTER!")
+#4
+if 1 < 2 and 4 > 2:
+    print("Math is fun")
+#better
+if 1 < 2 < 4:
+    print("Math is fun")
+
+#5
