d7e8bf4cf905c30decbb6f9180bb8dbb3b354bcf
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-06 10:14:02 +0530

    boolean comparisons

--- Fundamentals1/boolean_logic.py
+++ Fundamentals1/boolean_logic.py
@@ -21,3 +21,40 @@ else:
 user = 'Mark'
 if user == "Mark":
     print("Awesome Sauce")
+
+
+# Python also allows you to use words like or, and, and not for comparison
+if 1 > 2 or 2 > 1:
+    print("cool")
+
+if 1 == 1 and 2 == 2:
+    print("nice!")
+
+if not False: #not false == true
+    print("it is true!")
+
+# One nice thing about comparing numbers is that you can string inequalities
+# together without using the and keyword:
+if 1 < 2 and 2 < 3:
+    print("this is ok")
+
+if 1 < 2 < 3:
+    print("this is better!")
+
+# Also, it's important to understand that the indentation in all of
+# these examples matters tremendously. Indentation is how Python keeps
+# track of what code should be executed conditionally, and which code
+# should always be executed:
+
+
+name = "Mark"
+if name == "Mark":
+    print("Your name is Mark")
+print("Bye!")
+# In the example above, both messages will be printed. But if you change
+# the name variable to something besides "Mark", the second print
+# statement will still execute.
+
+# name = "Matt"
+# if name == "Matt":
+# print("Your name is Matt") # IndentationError!
