08aa5a1168f26f88ee0f1f596a46ad70e0adb831
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-07 11:54:46 -0700

    variables and primatives

--- Fundamentals1/variables_and_primatives.py
+++ Fundamentals1/variables_and_primatives.py
@@ -10,3 +10,11 @@ b # 2
 # variables in snake case
 my_variable = 3
 my_variable2 = 4
+
+#types
+type(False) # bool
+type("nice") # string
+type({}) # dict
+type([]) # class list
+type(()) # class tuple - Tuples are lists with immutable values - uses parentheses (4, 2, 1)
+type(None) # class NoneType 
