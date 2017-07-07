7be6b2ed9568506dc3a98e2cdb02da6b5fb20113
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-07 13:25:29 -0700

    unicode

--- Fundamentals1/variables_and_primatives.py
+++ Fundamentals1/variables_and_primatives.py
@@ -20,3 +20,19 @@ type(()) # class tuple - Tuples are lists with immutable values - uses parenthes
 type(None) # class NoneType
 
 # Strings - in Python 3, all strings are represented in Unicode.
+# The most popular character encoding that is dominant on the web now is
+# UTF-8, which uses 8-bit code units, but with a variable length to ensure
+# that all sorts of characters can be encoded.
+
+# Then there are languages like Chinese, Japanese, and Korean, which
+# have so many characters that they require multiple-byte character sets.
+# That is, each “character” is represented by a two-byte number from 0–65535
+# Two bytes is 2 ** 16 = 65536
+
+# Well, systems had to be designed to carry encoding information along
+# with every piece of “plain text.” Remember, it’s the decryption key
+# that maps computer-readable numbers to human-readable characters. A
+# missing decryption key means garbled text, gibberish, or worse.
+
+# UNICODE 
+# Unicode is a system designed to represent every character from every language.
