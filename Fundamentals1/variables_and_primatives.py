9883bb70becb5abb743ddcb787220eeca0c3c0b1
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-07 13:57:17 -0700

    unicode

--- Fundamentals1/variables_and_primatives.py
+++ Fundamentals1/variables_and_primatives.py
@@ -34,5 +34,46 @@ type(None) # class NoneType
 # that maps computer-readable numbers to human-readable characters. A
 # missing decryption key means garbled text, gibberish, or worse.
 
-# UNICODE 
+# UNICODE
 # Unicode is a system designed to represent every character from every language.
+
+# Unicode represents each letter, character, or ideograph as a 4-byte
+# number. Each number represents a unique character used in at least one
+# of the world’s languages. (Not all the numbers are used, but more than
+# 65535 of them are, so 2 bytes wouldn’t be sufficient.)
+
+# 4 bytes is  2 ** 32 = 4294967296
+
+# Characters that are used in multiple languages generally have the same
+# number, unless there is a good etymological reason not to. Regardless,
+# there is exactly 1 number per character, and exactly 1 character per
+# number. Every number always means just one thing; there are no “modes”
+# to keep track of. U+0041 is always 'A', even if your language doesn’t
+# have an 'A' in it.
+
+# Four bytes? For every single character‽ That seems awfully wasteful,
+# especially for languages like English and Spanish, which need less than
+# one byte (256 numbers) to express every possible character.
+
+# There is a Unicode encoding that uses four bytes per character. It’s
+# called UTF-32, because 32 bits = 4 bytes. UTF-32
+# This has some advantages, the most important being that you can find
+# the Nth character of a string in constant time, because the Nth
+# character starts at the 4×Nth byte
+# DISADVANTAGE - 4 bytes per character file size
+
+#Even though there are a lot of Unicode characters, it turns out that
+# most people will never use anything beyond the first 65535.
+
+# UTF-16 (because 16 bits = 2 bytes). UTF-16 encodes every character
+# from 0–65535 as two bytes
+# twice as space-efficient as UTF-32
+#you can still easily find the Nth character of a string in constant time
+# if you assume that the string doesn’t include any astral plane characters,
+# which is a good assumption right up until the moment that it’s not.
+
+# But there are also non-obvious disadvantages to both UTF-32 and UTF-16.
+# Different computer systems store individual bytes in different ways.
+# To solve this problem, the multi-byte Unicode encodings define a “Byte
+# Order Mark,” which is a special non-printable character that you can
+# include at the beginning of your document to indicate what order your bytes are in.
