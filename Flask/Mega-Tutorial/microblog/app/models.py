679b31a91647ba6e04ffeab614e744bf85f97fa4
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-08 13:44:23 +0530

    microblog create models

---
+++ Flask/Mega-Tutorial/microblog/app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+# The __repr__ method tells Python how to print objects of this class.
+# We will use this for debugging.
