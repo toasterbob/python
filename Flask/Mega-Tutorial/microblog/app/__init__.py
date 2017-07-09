b275681ffaa55fdc43db51e9c1373b5dfddee88e
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-09 18:56:53 +0530

    microblog __init__.py

--- Flask/Mega-Tutorial/microblog/app/__init__.py
+++ Flask/Mega-Tutorial/microblog/app/__init__.py
@@ -1,9 +1,18 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+# The Flask-OpenID extension requires a path to a temp folder where files
+# can be stored. For this we provide the location of our tmp folder.
 
 from app import views, models
 
