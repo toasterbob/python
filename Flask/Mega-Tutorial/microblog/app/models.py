64409489d718b08c7004bbc12c10630801dbed3f
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-09 18:16:52 +0530

    microblog add Post model and update relationship on User

--- Flask/Mega-Tutorial/microblog/app/models.py
+++ Flask/Mega-Tutorial/microblog/app/models.py
@@ -4,8 +4,21 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
-# The __repr__ method tells Python how to print objects of this class.
-# We will use this for debugging.
+    # The __repr__ method tells Python how to print objects of this class.
+    # We will use this for debugging.
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
+
+# The user_id field in the Post class was initialized as a foreign key,
+# so that Flask-SQLAlchemy knows that this field will link to a user.
