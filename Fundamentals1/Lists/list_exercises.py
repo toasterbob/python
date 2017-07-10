e74f6121dbe2a6bcad0c7978886d55088228b3e4
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-10 11:38:02 -0700

    list exercises: list comprehension

--- Fundamentals1/Lists/list_exercises.py
+++ Fundamentals1/Lists/list_exercises.py
@@ -1,12 +1,14 @@
-
+#List Comprehension
 #1
 list = [1, 2, 3, 4]
 for num in list:
     print(num)
 
-# theirs 
+# theirs
 [print(val) for val in [1,2,3,4]]
 
+#mine
+[print(num) for num in [1, 2, 3, 4]]
 
 #2
 list = [num * 20 for num in range(1,5)]
@@ -19,3 +21,11 @@ for num in list:
 
 # theirs
 [print(val*20) for val in [1,2,3,4]]
+
+#mine
+[print(num * 20) for num in [1, 2, 3, 4]]
+
+#3
+[name[0] for name in ["Elie", "Tim", "Matt"]]
+
+#4
