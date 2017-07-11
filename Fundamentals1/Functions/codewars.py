bad7e6862914bbac6a73edb7c03022e85c461cf6
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-12 02:14:16 +0530

    codewars: looking for a benefactor

--- Fundamentals1/Functions/codewars.py
+++ Fundamentals1/Functions/codewars.py
@@ -1,1 +1,9 @@
 # Looking for a benefactor
+def new_avg(arr, newavg):
+    if arr[-1] <= 0:
+        raise ValueError('Last value is not positive')
+    target = (len(arr) + 1) * newavg
+    donation = target - sum(arr)
+    if donation <= 0:
+        raise ValueError('Donation is not positive')
+    return donation
