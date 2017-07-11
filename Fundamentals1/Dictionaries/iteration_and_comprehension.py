851ee793014274b48cfde8c7648d0e6aa279038d
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-10 18:11:09 -0700

    dictionary comprehension

--- Fundamentals1/Dictionaries/iteration_and_comprehension.py
+++ Fundamentals1/Dictionaries/iteration_and_comprehension.py
@@ -18,3 +18,39 @@ for key, value in d.items():
 
 # name:Elie
 # job:Instructor
+
+
+
+# Dictionary comprehension
+
+# - We can convert dictionaries into lists using list comprehension
+d = {'a': 1, 'c': 3, 'e': 5}
+[v for k,v in d.items()] # [1, 5, 3]
+[k for k,v in d.items()] # ['a', 'e', 'c']
+
+[[k,v] for k,v in d.items()] # [['a', 1], ['c', 3], ['e', 5]]
+
+
+# But we can also convert other data types into dictionaries using dictionary comprehension!
+str1 = "ABC"
+str2 = "123"
+{str1[i]: str2[i] for i in range(0,len(str1))} # {'A': '1', 'B': '2', 'C': '3'}
+
+
+{ num:("even" if num % 2 == 0 else "odd") for num in range(1,5)
+# {1: 'odd', 2: 'even', 3: 'odd', 4: 'even'}
+
+# theirs
+num_list = [1,2,3,4]
+{ num:("even" if num % 2 == 0 else "odd") for num in num_list }
+
+
+
+
+
+
+
+
+
+
+#
