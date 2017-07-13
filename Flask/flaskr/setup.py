ccc0be9b2a2ebcfbffc905c0b29835f82b53c37f
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-13 13:28:18 +0530

    Flaskr setup.py

--- Flask/flaskr/setup.py
+++ Flask/flaskr/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='flaskr',
     packages=['flaskr'],
-    include_package_date=True,
+    include_package_data=True,
     install_requires=[
         'flask',
     ],
