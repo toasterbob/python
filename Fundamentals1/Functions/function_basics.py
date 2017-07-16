38075ae56d7634740ffbddf9c022c727f4e71880
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-15 20:53:50 -0700

    Function basics

--- Fundamentals1/Functions/function_basics.py
+++ Fundamentals1/Functions/function_basics.py
@@ -10,3 +10,10 @@ print(add_five_plus_five())
 
 ten = add_five_plus_five()
 print(ten + 10) #20
+
+def return_none():
+    "hi" #no return
+
+var = return_none();
+print(var) # None
+print(var == None) # true
