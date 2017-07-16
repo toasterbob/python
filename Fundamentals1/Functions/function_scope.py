b5717620ac9ddc8be7aa51c47738fcfe30300c1f
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-15 23:53:08 -0700

    Function Scope: the global keyword

--- Fundamentals1/Functions/function_scope.py
+++ Fundamentals1/Functions/function_scope.py
@@ -7,3 +7,24 @@ def func():
 
 func() # 5
 x # NameError
+
+
+# The global keyword
+# The global scope includes all variables defined outside of functions.
+
+# But if we try to use a global variable in a method, we will see
+# UnboundLocalError: local variable VARIABLE_NAME referenced before assignment
+
+# This happens because a method in Python either has local variables or
+# global variables. If variable is defined anywhere in a method and that
+# variable has the same name as a global variable, then the new local
+# variable will be used in the function instead of the global. But if you
+# actually want to assign a global variable from within a function, you
+# need to use the global keyword. Using global variables in general
+# is not best practice
+
+
+
+
+
+#
