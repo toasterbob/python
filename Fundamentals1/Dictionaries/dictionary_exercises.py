b1256f55050ef31167fe3202a38a489092bc494e
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-15 20:31:06 -0700

    Dictionary exercises

--- Fundamentals1/Dictionaries/dictionary_exercises.py
+++ Fundamentals1/Dictionaries/dictionary_exercises.py
@@ -27,4 +27,27 @@ dict(zip(a,b))
 string = "awesome sauce"
 
 {vowel: string.count(vowel) for vowel in "awesome sauce" if vowel in ["a","e","i","o","u"]}
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{ chr: "awesome sauce".count(chr) for chr in ["a", "e", "i", "o", "u"]}
