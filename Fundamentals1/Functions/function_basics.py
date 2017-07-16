38a8a89683e518f37df1fbbf89a5b3d84c44d35f
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-15 20:50:44 -0700

    Function basics

--- Fundamentals1/Functions/function_basics.py
+++ Fundamentals1/Functions/function_basics.py
@@ -2,3 +2,11 @@ def first_function():
     print("Hello World!")
 
 first_function() #"Hello World!"
+
+def add_five_plus_five():
+    return 5 + 5
+
+print(add_five_plus_five())
+
+ten = add_five_plus_five()
+print(ten + 10) #20
