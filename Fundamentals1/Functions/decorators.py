44c4abd0c5b4b3dd21d90ea49b095f0a175ffc92
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-17 14:27:59 -0700

    decorators

---
+++ Fundamentals1/Functions/decorators.py
@@ -0,0 +1,10 @@
+
+# First Class Objects
+
+# In Python, functions are first-class objects. This means that functions
+# can be passed around, and used as arguments, just like any other value
+
+
+
+
+#
