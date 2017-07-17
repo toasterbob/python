34834c79dd5654332cbf78bcda8b5a395fda545a
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-16 17:33:57 -0700

    Function exercises

--- Fundamentals1/Functions/function_exercises.py
+++ Fundamentals1/Functions/function_exercises.py
@@ -5,12 +5,21 @@ difference(2,2) # 0
 difference(0,2) # -2
 
 
+def product(a, b):
+    return a * b
+
+product(2,2) # 4
+product(0,2) # 0
 
 
+def print_day(a: int):
+    if a < 1 or a > 7:
+        return None
+    days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    return days[a - 1]
 
-
-
-
+print_day(4) # "Wednesday"
+print_day(41) # None
 
 
 
