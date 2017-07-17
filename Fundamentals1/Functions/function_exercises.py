09f3331dbfc91578f5ec2691df66ee3116d2f333
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-16 23:05:25 -0700

    Function exercises

--- Fundamentals1/Functions/function_exercises.py
+++ Fundamentals1/Functions/function_exercises.py
@@ -28,9 +28,26 @@ def print_day(n):
         return ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][n - 1]
     except IndexError as e:
         return None
-        
+
 print_day(4) # "Wednesday"
 print_day(41) # None
+
+
+def last_element(arr):
+    try:
+        return arr[-1]
+    except IndexError as e:
+        return None
+
+last_element([1,2,3,4]) # 4
+last_element([]) # None
+
+
+
+
+
+
+
 
 
 #
